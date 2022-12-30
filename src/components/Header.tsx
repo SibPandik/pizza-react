5b4c27f6cd41dea25375718063f18cf6548b805c
Кир Кот <mr.kot.2018@mail.ru>
2022-12-30 22:54:17 +0300

    Make some fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-//import React from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -8,8 +8,17 @@ import { selectCart } from '../redux/slices/cartSlice';
 
 function Header() {
   const {items, totalPrice} = useSelector(selectCart);
+  const isMounted = React.useRef(false);
 
   const totalCount = items.reduce((sum: number, item: any) => sum + item.count, 0);
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const json = JSON.stringify(items);
+      localStorage.setItem('cart', json);
+    }
+    isMounted.current = true;
+  }, [items]);
 
   return (
     <div className="header">
